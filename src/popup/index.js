9a3faffa4c761937e54d07b9f2fa409d6bf7a513
songhongbao <s1716082907@gmail.com>
2025-03-27 16:16:11 +0800

    feat: 引入UI框架vuetify

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import Popup from './Popup.vue'
 
-createApp(Popup).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(Popup).use(vuetify).mount('#app')
